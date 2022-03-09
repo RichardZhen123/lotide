7f2169064a225158003e1f3fcbe7a0e8ec7dd3bf
RichardZhen123 <richardzhen5555@gmail.com>
2022-03-09 09:09:58 +0000

    implement without.js

---
+++ without.js
@@ -0,0 +1,48 @@
+const assertArraysEqual = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return console.log(`❌❌❌ Assertion Failed: ${arr1} !== ${arr2}`);
+    }
+  }
+
+  return console.log(`✅✅✅ Assertion Pased: ${arr1} === ${arr2}`);
+}
+
+const eqArrays = function (arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const without = function(source, itemsToRemove) {
+  let x = 0;
+
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] === itemsToRemove[j]) {
+        x += i;
+      }
+    }
+  }
+
+  let copyOfSource = source.slice ();
+
+  copyOfSource.splice(x,1);
+  console.log(copyOfSource);
+}
+
+
+assetwithout([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
