17b2beaa6326a97af274ee43309e0693ab606739
RichardZhen123 <richardzhen5555@gmail.com>
2022-03-10 00:59:14 +0000

    Implement countLetter.js

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Pased: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let individLetters = sentence.split("");
+  const result = {};
+  let total = 0;
+  firstLetter = "";
+
+  for (let i = 0; i < individLetters.length; i++) {
+    if (firstLetter !== individLetters[i]) {
+      firstLetter = individLetters[i];
+      result[firstLetter] = total;
+    }
+    if (firstLetter === individLetters[i]) {
+      result[individLetters[i]]++
+    }
+  }
+  return result;
+}
+
+console.log(countLetters('aaabbbccc'));
